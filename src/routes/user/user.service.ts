a5739be0954a5dbc8ac598cfb1b3d95589905ee7
leminhduc6402 <leminhduc6402@gmail.com>
2026-04-29 15:55:27 +0700

    refactor: modularize role authorization logic and standardize self-update/delete checks in UserService

--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,34 +1,36 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/routes/user/user.repo';
+import { CreateUserBodyType, GetUsersQueryType, UpdateUserBodyType } from 'src/routes/user/user.model';
+import { NotFoundRecordException } from 'src/shared/error';
+import { isForeignKeyConstraintError, isNotFoundError, isUniqueConstraintError } from 'src/shared/helpers';
 import {
   CannotUpdateOrDeleteYourselfException,
   RoleNotFoundException,
   UserAlreadyExistsException,
 } from 'src/routes/user/user.error';
-import { CreateUserBodyType, GetUsersQueryType, UpdateUserBodyType } from 'src/routes/user/user.model';
-import { UserRepository } from 'src/routes/user/user.repo';
 import { RoleName } from 'src/shared/constants/role.constant';
-import { NotFoundRecordException } from 'src/shared/error';
-import { isForeignKeyConstraintError, isNotFoundError, isUniqueConstraintError } from 'src/shared/helpers';
-import { SharedRoleRepository } from 'src/shared/repositories/shared-role.repo';
 import { SharedUserRepository } from 'src/shared/repositories/shared-user.repo';
 import { HashingService } from 'src/shared/services/hashing.service';
+import { SharedRoleRepository } from 'src/shared/repositories/shared-role.repo';
 
 @Injectable()
 export class UserService {
   constructor(
-    private readonly userRepository: UserRepository,
-    private readonly hashingService: HashingService,
-    private readonly sharedUserRepository: SharedUserRepository,
-    private readonly sharedRoleRepository: SharedRoleRepository,
+    private UserRepository: UserRepository,
+    private hashingService: HashingService,
+    private sharedUserRepository: SharedUserRepository,
+    private sharedRoleRepository: SharedRoleRepository,
   ) {}
 
-  async findAll(pagination: GetUsersQueryType) {
-    const data = await this.userRepository.findAll(pagination);
-    return data;
+  findAll(pagination: GetUsersQueryType) {
+    return this.UserRepository.findAll(pagination);
   }
 
   async findById(id: number) {
-    const user = await this.sharedUserRepository.findUniqueIncludeRolePermissions({ id, deletedAt: null });
+    const user = await this.sharedUserRepository.findUniqueIncludeRolePermissions({
+      id,
+      deletedAt: null,
+    });
     if (!user) {
       throw NotFoundRecordException;
     }
@@ -45,23 +47,50 @@ export class UserService {
     createdByRoleName: string;
   }) {
     try {
-      await this.verifyRole({ roleNameAgent: createdByRoleName, roleIdTarget: data.roleId });
+      // Chỉ có admin agent mới có quyền tạo user với role là admin
+      await this.verifyRole({
+        roleNameAgent: createdByRoleName,
+        roleIdTarget: data.roleId,
+      });
+      // Hash the password
       const hashedPassword = await this.hashingService.hash(data.password);
-      return await this.userRepository.create({
+
+      const user = await this.UserRepository.create({
         createdById,
         data: {
           ...data,
           password: hashedPassword,
         },
       });
+      return user;
     } catch (error) {
       if (isForeignKeyConstraintError(error)) {
         throw RoleNotFoundException;
       }
+
       if (isUniqueConstraintError(error)) {
         throw UserAlreadyExistsException;
       }
       throw error;
+    }
+  }
+
+  /**
+   * Function này kiểm tra xem người thực hiện có quyền tác động đến người khác không.
+   * Vì chỉ có người thực hiện là admin role mới có quyền sau: Tạo admin user, update roleId thành admin, xóa admin user.
+   * Còn nếu không phải admin thì không được phép tác động đến admin
+   */
+  private async verifyRole({ roleNameAgent, roleIdTarget }) {
+    // Agent là admin thì cho phép
+    if (roleNameAgent === RoleName.Admin) {
+      return true;
+    } else {
+      // Agent không phải admin thì roleIdTarget phải khác admin
+      const adminRoleId = await this.sharedRoleRepository.getAdminRoleId();
+      if (roleIdTarget === adminRoleId) {
+        throw new ForbiddenException();
+      }
+      return true;
     }
   }
 
@@ -77,15 +106,27 @@ export class UserService {
     updatedByRoleName: string;
   }) {
     try {
-      if (id === updatedById) {
-        throw CannotUpdateOrDeleteYourselfException;
-      }
-      const currentUser = await this.sharedUserRepository.findUnique({ id, deletedAt: null });
-      if (!currentUser) {
-        throw NotFoundRecordException;
-      }
-      await this.verifyRole({ roleNameAgent: updatedByRoleName, roleIdTarget: currentUser.roleId });
-      const updatedUser = await this.sharedUserRepository.update({ id, deletedAt: null }, { updatedById, ...data });
+      // Không thể cập nhật chính mình
+      this.verifyYourself({
+        userAgentId: updatedById,
+        userTargetId: id,
+      });
+
+      // Lấy roleId ban đầu của người được update để kiểm tra xem liệu người update có quyền update không
+      // Không dùng data.roleId vì dữ liệu này có thể bị cố tình truyền sai
+      const roleIdTarget = await this.getRoleIdByUserId(id);
+      await this.verifyRole({
+        roleNameAgent: updatedByRoleName,
+        roleIdTarget,
+      });
+
+      const updatedUser = await this.sharedUserRepository.update(
+        { id, deletedAt: null },
+        {
+          ...data,
+          updatedById,
+        },
+      );
       return updatedUser;
     } catch (error) {
       if (isNotFoundError(error)) {
@@ -101,18 +142,41 @@ export class UserService {
     }
   }
 
+  private async getRoleIdByUserId(userId: number) {
+    const currentUser = await this.sharedUserRepository.findUnique({
+      id: userId,
+      deletedAt: null,
+    });
+    if (!currentUser) {
+      throw NotFoundRecordException;
+    }
+    return currentUser.roleId;
+  }
+
+  private verifyYourself({ userAgentId, userTargetId }: { userAgentId: number; userTargetId: number }) {
+    if (userAgentId === userTargetId) {
+      throw CannotUpdateOrDeleteYourselfException;
+    }
+  }
+
   async delete({ id, deletedById, deletedByRoleName }: { id: number; deletedById: number; deletedByRoleName: string }) {
     try {
-      if (id === deletedById) {
-        throw CannotUpdateOrDeleteYourselfException;
-      }
-      const currentUser = await this.sharedUserRepository.findUnique({ id, deletedAt: null });
-      if (!currentUser) {
-        throw NotFoundRecordException;
-      }
-      await this.verifyRole({ roleNameAgent: deletedByRoleName, roleIdTarget: currentUser.roleId });
+      // Không thể xóa chính mình
+      this.verifyYourself({
+        userAgentId: deletedById,
+        userTargetId: id,
+      });
 
-      await this.userRepository.delete({ id, deletedById });
+      const roleIdTarget = await this.getRoleIdByUserId(id);
+      await this.verifyRole({
+        roleNameAgent: deletedByRoleName,
+        roleIdTarget,
+      });
+
+      await this.UserRepository.delete({
+        id,
+        deletedById,
+      });
       return {
         message: 'Delete successfully',
       };
@@ -122,16 +186,5 @@ export class UserService {
       }
       throw error;
     }
-  }
-
-  private async verifyRole({ roleNameAgent, roleIdTarget }) {
-    if (roleNameAgent === RoleName.Admin) {
-      return true;
-    }
-    const adminRoleId = await this.sharedRoleRepository.getAdminRoleId();
-    if (roleIdTarget === adminRoleId) {
-      throw new ForbiddenException();
-    }
-    return true;
   }
 }
