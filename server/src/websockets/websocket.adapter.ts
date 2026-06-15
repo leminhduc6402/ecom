306396173f7f5c2815d1b6f2ba0d9c0aed809495
leminhduc6402 <leminhduc6402@gmail.com>
2026-06-15 15:11:34 +0700

    refactor: apply authMiddleware globally to all WebSocket namespaces using a regex pattern

--- server/src/websockets/websocket.adapter.ts
+++ server/src/websockets/websocket.adapter.ts
@@ -1,20 +1,31 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { Server, ServerOptions, Socket } from 'socket.io';
 
+const namespaces = ['/', 'payment', 'chat'];
 export class WebSocketAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions) {
-    const server = super.createIOServer(port, { ...options, cors: { origin: '*', credentials: true } });
-    // server.use(this.authMiddleware);
-
-    const authMiddleware = (socket: Socket, next: (err?: Error) => void) => {
+    const server: Server = super.createIOServer(port, {
+      ...options,
+      cors: {
+        origin: '*',
+        credentials: true,
+      },
+    });
+    const authMiddleware = (socket: Socket, next: (err?: any) => void) => {
+      console.log('connected', socket.id);
       socket.on('disconnect', () => {
-        console.log('Socket disconnected: ', socket.id);
+        console.log(`Client disconnected: ${socket.id}`);
       });
       next();
     };
-
-    server.of('payment').use(authMiddleware);
-    server.of('chat').use(authMiddleware);
+    server.use(authMiddleware);
+    server.of(/.*/).use(authMiddleware);
+    // namespaces.forEach((item) => {
+    //   server.of(item).use(authMiddleware)
+    // })
+    // server.use(authMiddleware)
+    // server.of('payment').use(authMiddleware)
+    // server.of('chat').use(authMiddleware)
     return server;
   }
 }
